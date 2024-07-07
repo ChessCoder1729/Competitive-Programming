9940ef283109ba5ee26cd2887dac475ec56a5bb7
ChessCoder1729 <chesscoder1729@gmail.com>
2024-07-07 15:18:34 +0530

    initial commit

--- CSES/Dynamic Programming/Solver.cpp
+++ CSES/Dynamic Programming/Solver.cpp
@@ -1,50 +1,33 @@
 #include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
 using namespace std;
+using namespace __gnu_pbds;
+template <class T>
+using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
 #define endl '\n';
 #define fastio ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0);
 #define py cout << "YES" << endl;
 #define pn cout << "NO" << endl;
+#define pb push_back
 const long long inf = 1e18;
 const long long mod = 1e9+7;
-const int n = 1e6  + 1;
+const int N = 2e5;
 typedef long long ll;
 typedef unsigned long long ull;
 typedef long double lld;
-// to comment multiple lines at once ctrl+/
 
-// Find and replace Ctrl+H
-map<pair<int,int>,ll>dp;
-
-int n, x;
-
-ll solve(int target, int last, vector<int>&c){
-    if(target==0){
-        return 1;
-    }
-    if(dp[{target,last}]){
-        return dp[{target,last}];
-    }
-    ll ans = 0;
-    for(int i = last;i<n;i++){
-        if(target>=c[i]){
-            ans += solve(target-c[i],i,c);
-            ans = ans%mod;
-        }
-        else{
-            break;
-        }
-    }
-    dp[{target,last}] = ans;
-    return ans;
+void judge(){
+    srand(time(NULL));
+    #ifndef ONLINE_JUDGE
+    freopen("1.txt","r",stdin);
+    freopen("2.txt","w",stdout);
+    #endif
 }
+ 
+// to comment multiple lines at once ctrl+/
+// Find and replace Ctrl+H
 
 int main(){
-    fastio;
-    //freopen("1.txt","r",stdin);
-    //freopen("2.txt","w",stdout);
-    cin >> n >> x; vector<int>c(n);
-    for(int i = 0;i<n;i++) cin >> c[i];
-    sort(c.begin(),c.end());
-    //cout << x << ' ';
-    cout << solve(x,0,c) << ' ';
+    fastio; judge();
+    
 }
