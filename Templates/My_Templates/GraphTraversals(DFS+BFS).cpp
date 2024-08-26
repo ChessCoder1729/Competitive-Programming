05e7b86ea9e3231d0d71ddb104a248357fe55e05
ChessCoder1729 <chesscoder1729@gmail.com>
2024-08-26 20:45:54 +0530

    Initial commit

--- Templates/My_Templates/GraphTraversals(DFS+BFS).cpp
+++ Templates/My_Templates/GraphTraversals(DFS+BFS).cpp
@@ -1,24 +1,33 @@
-// DFS
-void dfs(int node, vector<vector<int>>&adj, vector<bool>&vis){
-    vis[node] = true;
-    for(auto child : adj[node]){
-        if(!vis[child]){
-            dfs(child,adj,vis);
+struct Graph{
+    vector<bool>vis; vector<vector<int>>adj;
+    void build(int n, int m){
+        vis.resize(n); adj.resize(n); int u,v;
+        for(int i = 0;i<m;i++){
+            cin >> u >> v; u--; v--;// Check if 0-based index or not
+            adj[u].push_back(v); adj[v].push_back(u);
         }
     }
-}
-
-// BFS
-void bfs(int start,vector<vector<int>>&adj, vector<bool>&vis){
-    queue<int>q; q.push(start);
-    vis[start] = true;
-    while(!q.empty()){
-        int node = q.front();
-        q.pop();
-        for(auto child:node){
+    //DFS
+    void dfs(int node){
+        vis[node] = true;
+        for(auto child : adj[node]){
             if(!vis[child]){
-                q.push(child);
+                dfs(child);
             }
         }
     }
-}
+    // BFS
+    void bfs(int start){
+        queue<int>q; q.push(start);
+        vis[start] = true;
+        while(!q.empty()){
+            int node = q.front();
+            q.pop();
+            for(auto child:adj[node]){
+                if(!vis[child]){
+                    q.push(child);
+                }
+            }
+        }
+    }
+};
